c75b0857d12bc4099513a9a8d5033da4ff39ce81
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-25 01:07:51 -0500

    feat: add cell component

--- src/components/cell/index.stories.tsx
+++ src/components/cell/index.stories.tsx
@@ -1,8 +1,72 @@
 import React from 'react'
+import { boolean } from '@storybook/addon-knobs'
+import ReminderPreview from '../reminder-preview'
 import Cell from './index'
 
 export default { title: 'Cell' }
 
 export const Default = () => {
-  return <Cell />
+  const isToday = boolean('isToday', false)
+
+  return (
+    <Cell date={new Date()} isToday={isToday}>
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#efdcee"
+        text="HR Interview"
+        className="mt-2 mb-2"
+      />
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#efdc00"
+        text="Family Launch"
+        className="mb-2"
+      />
+    </Cell>
+  )
+}
+
+export const Overflow = () => {
+  const isToday = boolean('isToday', false)
+
+  return (
+    <Cell date={new Date()} isToday={isToday}>
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#efdcee"
+        text="HR Interview"
+        className="mt-2 mb-2"
+      />
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#efdc00"
+        text="Family Launch"
+        className="mb-2"
+      />
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#ee00ee"
+        text="Go to cinema"
+        className="mb-2"
+      />
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#db4968"
+        text="Go to cinema"
+        className="mb-2"
+      />
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#68c583"
+        text="Go to cinema"
+        className="mb-2"
+      />
+      <ReminderPreview
+        dueDate={new Date()}
+        bgColor="#9453a3"
+        text="Go to cinema"
+        className="mb-2"
+      />
+    </Cell>
+  )
 }
--- src/components/cell/index.tsx
+++ src/components/cell/index.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import format from 'date-fns/format'
+import cs from 'classnames'
 
-const Cell = () => {
+export interface CellProps {
+  date: Date
+  isToday?: boolean
+  className?: string
+}
+
+const Cell: React.FC<CellProps> = ({
+  className,
+  date,
+  isToday = false,
+  children
+}) => {
   return (
-    <div className="border border-gray-300 bg-yellow-200">Hello world!</div>
+    <div
+      role="button"
+      className={cs(
+        'flex flex-col border border-border bg-cell p-2',
+        className
+      )}
+      style={{ minHeight: 166, maxHeight: 166 }}
+    >
+      <div
+        className={cs(
+          'rounded-full h-8 w-8 flex flex-shrink-0 items-center justify-center self-end text-right',
+          {
+            'bg-red-700 text-cell': isToday
+          }
+        )}
+      >
+        {format(date, 'dd')}
+      </div>
+      {children && (
+        <ul className="mt-2 flex flex-col min-h-0 overflow-auto">{children}</ul>
+      )}
+    </div>
   )
 }
 
