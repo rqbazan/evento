64f5cb09d787b0c1ff9694bd3ce4dfd888bc3fb0
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-25 01:08:27 -0500

    feat: add col-header component

---
+++ src/components/col-header/index.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ColHeader from './index'
+
+export default { title: 'ColHeader' }
+
+export const Default = () => {
+  return <ColHeader>Sunday</ColHeader>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        cell: '#FFFCF4',
+        border: '#D4D4D4',
+        accent: {
+          100: '#3302F9',
+          700: '#180174'
+        }
+      }
+    }
   },
   variants: {},
   plugins: []
