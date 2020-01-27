f157d7b9903d51d1bf8a94254d215c7d7cdda033
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-27 16:12:03 -0500

    feat: add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import cs from 'classnames'
+
+export type ButtonProps = React.HTMLAttributes<HTMLButtonElement>
+
+const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
+  return (
+    <button
+      type="button"
+      className={cs(
+        'h-10 bg-indigo-600 hover:bg-indigo-700 text-white px-4 rounded focus:outline-none',
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export default Button
